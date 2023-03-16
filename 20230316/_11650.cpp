64877ccea25c19d448b86b27e8e92f88d7e18f80
Minsik Kim <ppp37686@gmail.com>
2023-03-16 21:33:37 +0900

    added 11650

---
+++ 20230316/_11650.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<pair<int, int>> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i].first >> arr[i].second;
+    sort(arr.begin(), arr.end());
+	for (int i = 0; i < n; i++)
+		cout << arr[i].first << ' ' << arr[i].second << '\n';  
+}
