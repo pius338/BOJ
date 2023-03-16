f8111667289ecff26ace340567edd33cf5acd2c0
Minsik Kim <ppp37686@gmail.com>
2023-03-16 19:45:38 +0900

    added 11729

--- 20230316/_11729.cpp
+++ 20230316/_11729.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int count = 0;
 
-void hanoi(int n, int src, int dest, int temp)
+void hanoi(int n, int src, int dest, int temp, vector<pair<int, int >> &arr)
 {
     if(n == 0)
-    {
-        cout << count << '\n';
         return ;
-    }
     else
     {
-        hanoi(n - 1, src, temp, dest);
-        cout << src << " " << dest << '\n';
+        hanoi(n - 1, src, temp, dest, arr);
+        arr.push_back(make_pair(src, dest));
         count++;
-        hanoi(n - 1, temp, dest, src);
+        hanoi(n - 1, temp, dest, src, arr);
     }
     
 }
@@ -24,6 +22,9 @@ int main()
 {
     int n;
     cin >> n;
-    hanoi(n, 1, 3, 2);
-    
+    vector<pair<int, int >> arr;
+    hanoi(n, 1, 3, 2, arr);
+    cout << count << '\n';
+	for (int i = 0; i < arr.size(); i++)
+		cout << arr[i].first << ' ' << arr[i].second << '\n';  
 }
