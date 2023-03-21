22d900ff0586a0c14e3439ba033499753cc4d9b0
Minsik Kim <ppp37686@gmail.com>
2023-03-22 00:40:17 +0900

    added 2751

---
+++ 20230321/_2751.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+	vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	sort(arr.begin(), arr.end());
+	for (int i = 0; i < arr.size(); i++)
+		cout << arr[i] << '\n';	
+}
