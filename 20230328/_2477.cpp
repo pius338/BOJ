49aba314f6df10ffb65a722f368f0d8a81f2c732
Minsik Kim <ppp37686@gmail.com>
2023-03-28 21:28:04 +0900

    added 2477

--- 20230328/_2477.cpp
+++ 20230328/_2477.cpp
@@ -3,17 +3,47 @@ using namespace std;
 
 int main()
 {
-    int n, m, l, t_w = 0;
-    vector<vector<int>> arr(5);l;
+    int n, m, l, size, flag = 0, t_l = 0, t_w = 0;
+    vector<int> arr(6);
+    deque<int> que;
+    cin >> n;
     for (int i = 0; i < 6; i++)
     {
         cin >> m >> l;
-        arr[m].push_back(l);
+        que.push_back(m);
+        arr[i] = l;
+        if(m == 1)
+            t_w += l;
+        if(m == 3)
+            t_l += l;
+
     }
-    
-    for (int i = 0; i < arr[1].size(); i++)
-        t_w += arr[1][i];
-    
-    
-    
+    int x, fst, sec;
+    int i = 0;
+    while(1)
+    {
+        x = que[0];
+        que.push_back(que[0]);
+        if(x == que[2] && flag)
+        {
+            if(que[1] == que[3])
+                fst = (i + 2 > 5) ? (i + 2) % 6 : i + 2;
+            else
+                fst = (i > 5) ? i % 6 : i;
+            break;
+        }
+        if(x == que[2])
+        {
+            if(que[1] != que[3])
+                sec = (i > 5) ? i % 6 : i;
+            else
+                sec = (i + 2 > 5) ? (i + 2) % 6 : i + 2;
+            flag++;
+        }
+        que.pop_front();
+        i++;
+    }
+    cout << fst << ' ' << sec;
+    size = (t_w * t_l) - (arr[fst] * arr[sec]);
+    cout << size * n;
 }
