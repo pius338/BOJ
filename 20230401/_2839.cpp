392bb307bc25f16687cccd82f5b07f9187cffe88
Minsik Kim <ppp37686@gmail.com>
2023-04-01 19:38:45 +0900

    added 2839

---
+++ 20230401/_2839.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m, a = 0, result = -1;
+	cin >> n;
+	m = n;
+	while(n > 0 && m)
+	{
+		m = n;
+		m %= 5;
+		if(m % 3)
+		{
+			n -= 3;
+			a++;
+		}
+		else
+			result = n / 5 + m / 3 + a;
+		m %= 3;
+	}
+	cout << result;
+}
--- 20230401/_7568.cpp
+++ 20230401/_7568.cpp
@@ -7,19 +7,12 @@ int main() {
 	cin >> n;
 	vector<pair<int, int>> arr(n);
 	vector<int> r_arr(n, 0);
-	vector<int> rr_arr(n);
 	for (int i = 0; i < n; i++)
-	{
 		cin >> arr[i].first >> arr[i].second;
-	}
 	for (int i = 0; i < n; i++)
-	{
 		for (int j = 0; j < n; j++)
-		{
 			if((arr[i].first < arr[j].first && arr[i].second < arr[j].second))
 				r_arr[i]++;
-		}
-	}
 	for (int i = 0; i < n; i++)
 		cout << r_arr[i] + 1<< ' ';
 }
