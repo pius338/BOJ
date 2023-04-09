64f023d48cc4d87dee08359c6299633f3fc0fa4f
Minsik Kim <ppp37686@gmail.com>
2023-04-10 08:42:59 +0900

    added dir

---
+++ 20230410/_1463.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+    vector<int> dp(1000001, -1);
+	dp[1] = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + 1;
+		if (i % 3 == 0 && dp[i / 3] != -1)
+			dp[i] = min(dp[i], dp[i/3] + 1);
+		if (i % 2 == 0 && dp[i / 2] != -1)
+			dp[i] = min(dp[i], dp[i/2] + 1);
+	}
+	cout << dp[n];
+}
