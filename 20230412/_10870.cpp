c10b9f4102ac589c61c89f07dd33c4a2a17a3dc8
Minsik Kim <ppp37686@gmail.com>
2023-04-13 02:47:58 +0900

    added 10870

---
+++ 20230412/_10870.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+    vector<int> arr(46);
+     for (int i = 1; i <= n; i++)
+    {
+        if(i <= 2)
+            arr[i] = 1;
+        if(i > 1)
+            arr[i] = arr[i - 2]+ arr[i - 1];
+    }
+    cout << arr[n];
+}
