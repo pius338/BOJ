d274a9dc0aec9b69e99eafb461d8ae85794af376
pius338 <49515751+pius338@users.noreply.github.com>
2023-04-15 20:40:02 +0900

    added 11047

---
+++ 20230415/_11047.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, k, cnt = 0;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (arr[i] <= k)
+        {
+            k -= arr[i];
+            cnt++;
+        }
+    }
+    cout << cnt;
+}
