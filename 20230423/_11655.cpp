ed929bcba7323470fcaacd28cf6b4950fd3d2924
Minsik Kim <ppp37686@gmail.com>
2023-04-23 12:54:05 +0900

    added 11655

---
+++ 20230423/_11655.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string str;
+	getline(cin, str);
+	for (int i = 0; i < str.size(); i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+			str[i] += 13;
+		else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
+			str[i] -= 13;
+	}
+	cout << str;
+}
