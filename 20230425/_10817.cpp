2e9ea39071cccf1f36b128a399f677767f129701
Minsik Kim <ppp37686@gmail.com>
2023-04-25 21:59:44 +0900

    added 10817

---
+++ 20230425/_10817.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	vector<int> arr(3);
+	for (int i = 0; i < 3; i++)
+		cin >> arr[i];
+	sort(arr.begin(), arr.end());
+	cout << arr[1];
+}
