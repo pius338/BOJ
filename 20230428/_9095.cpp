ee681982fbd55f726305e1db5fe7517a29776209
Minsik Kim <ppp37686@gmail.com>
2023-04-28 13:06:26 +0900

    added 5338

--- 20230428/_9095.cpp
+++ 20230428/_9095.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
 	int n;
 	cin >> n;
+	vector<int> darr(4);
+	darr[1] = 1; darr[2] = 2; darr[3] = 4;
+	vector<int> cntArr(4);
 	for (int i = 0; i < n; i++)
 	{
-		int m;
+		int m, nbr = 3;
 		cin >> m;
 		
 	}
