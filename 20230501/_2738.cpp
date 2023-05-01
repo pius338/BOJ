46a8445b340f3f5111764b18bdc57e584424a7c4
Minsik Kim <ppp37686@gmail.com>
2023-05-01 10:57:52 +0900

    added 2738

---
+++ 20230501/_2738.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> arr(n, vector<int> (m));
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			int a;
+			cin >> a;
+			arr[i][j] += a;
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << arr[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+}
