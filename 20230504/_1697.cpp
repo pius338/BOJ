97bcdaf16552beb08f7db6c04bde2fac9b45bbfd
Minsik Kim <ppp37686@gmail.com>
2023-05-04 11:37:36 +0900

    added 1697

---
+++ 20230504/_1697.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	bool isFound = false;
+	cin >> n >> k;
+	queue<int> que;
+	vector<int> isVisit(200001, 0);
+	que.push(n);
+	if(n == k)
+		isFound = true;	
+	if(n > k)
+	{
+		isVisit[k] = n - k;
+		isFound = true;
+	}
+	while (!que.empty() && !isFound)
+	{
+		int cur = que.front();
+		int dx[3] = {-1, 1, que.front()};
+		que.pop();
+		for (int i = 0; i < 3; i++)
+		{
+			int nx = cur + dx[i];
+			if(nx >= 0 && nx <= k + 1 && !isVisit[nx])
+			{
+				isVisit[nx] = isVisit[cur] + 1;
+				if(nx == k)
+					isFound = true;
+				que.push(nx);
+			}
+		}
+	}
+	cout << isVisit[k];
+}
