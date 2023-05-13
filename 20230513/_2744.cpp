9242e47eb2bb50b5bdcc0d1b73d86c194d786227
Minsik Kim <ppp37686@gmail.com>
2023-05-13 23:39:56 +0900

    added 2744

---
+++ 20230513/_2744.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if(str[i] >= 'a'&& str[i] <= 'z')
+            str[i] -= 32;
+        else if(str[i] >= 'A'&& str[i] <= 'Z')
+            str[i] += 32;
+        cout << str[i];
+    }
+}
