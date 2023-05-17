950017bdaab04baf926168799bd28cdef01ee303
Minsik Kim <ppp37686@gmail.com>
2023-05-17 13:58:01 +0900

    added 15652

---
+++ 20230517/_15652.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+
+void ft_bt(vector<int> &result, int sNbr, int cnt)
+{
+    result[cnt] = sNbr;
+    if(cnt == m - 1)
+    {
+        for (int i = 0; i < m; i++)
+            cout << result[i] << ' ';
+        cout << "\n";
+        return;
+    }
+    for (int i = sNbr; i < n + 1; i++)
+        ft_bt(result, i, cnt + 1);
+}
+
+int main()
+{
+    cin >> n >> m;
+    vector<int> result(m);
+    for (int i = 1; i <= n; i++)
+        ft_bt(result, i, 0);
+}
