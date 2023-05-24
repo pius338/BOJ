22c1284ab0a4cf8ab138eab93bb6d9d5283c1741
Minsik Kim <ppp37686@gmail.com>
2023-05-25 02:18:59 +0900

    added 5337

--- 20230525/_5337.cpp
+++ 20230525/_5337.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-  cout << ".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.";
+  std::cout << ".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.";
 }
