545578514767ff1ea14952db8f85c9646bbd0e5c
Minsik Kim <ppp37686@gmail.com>
2023-05-28 18:48:14 +0900

    added 1932

--- 20230528/_1932.cpp
+++ 20230528/_1932.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-	int n;
+	int n, m;
 	cin >> n;
-	for (int i = 0; i < n; i++)
+	vector<vector<int>> arr(n + 1, vector<int>(n, 0));
+	for (int i = 1; i < n + 1; i++)
 	{
-		/* code */
+		for (int j = 0; j < i; j++)
+		{
+			cin >> m;
+			if(j == 0)
+				arr[i][j] = arr[i - 1][j] + m;
+			else if(j == i - 1)
+				arr[i][j] = arr[i - 1][j - 1] + m;
+			else
+				arr[i][j] = max(arr[i - 1][j] + m, arr[i - 1][j - 1] + m);
+		}
 	}
-	
+	cout << *max_element(arr[n].begin(), arr[n].end());
 }
