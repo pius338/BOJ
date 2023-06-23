7cd5424a910b9af8bf159f0d74580030da4bce81
Minsik Kim <ppp37686@gmail.com>
2023-06-23 09:33:41 +0900

    added 14581

---
+++ 20230623/_14581.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+main(){
+    string str;
+    cin >> str;
+    cout << ":fan::fan::fan:\n" << ":fan::" << str << "::fan:\n" << ":fan::fan::fan:";
+}
