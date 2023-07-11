2911be902253caa03bd3d6678ab86cdc8c87447c
Minsik Kim <ppp37686@gmail.com>
2023-07-11 23:49:54 +0900

    added 27889

---
+++ 20230711/_27889.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	string str;
+	cin >> str;
+	if(str == "NLCS")
+		cout << "North London Collegiate School";
+	else if(str == "BHA")
+		cout << "Branksome Hall Asia";
+	else if(str == "KIS")
+		cout << "Korea International School";
+	else
+		cout << "St. Johnsbury Academy";
+}
