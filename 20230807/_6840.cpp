53edcb6c0a8edc7d60680e67e278a5bdf2375d47
Minsik Kim <ppp37686@gmail.com>
2023-08-07 01:35:10 +0900

    added 6840

---
+++ 20230807/_6840.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    vector<int> arr(3);
+    for(int i = 0; i < 3 ; i++)
+        cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    cout << arr[1];
+}
