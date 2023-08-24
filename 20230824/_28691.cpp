3bf0709e06aac9cb78acd06375c2af89c42c1ef5
Minsik Kim <ppp37686@gmail.com>
2023-08-24 21:28:44 +0900

    added 28691

---
+++ 20230824/_28691.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char c;
+    cin >> c;
+    switch (c)
+    {
+        case 'M':
+            cout << "MatKor";
+            break;
+        case 'W':
+            cout << "WiCys";
+            break;
+        case 'C':
+            cout << "CyKor";
+            break;
+        case 'A':
+            cout << "AlKor";
+            break;
+        case '$':
+            cout << "$clear";
+            break;
+    }
+}
