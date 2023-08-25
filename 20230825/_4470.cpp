64e010f160fe22131390a72b0d08f55011caa18a
Minsik Kim <ppp37686@gmail.com>
2023-08-25 23:41:33 +0900

    added 4470

---
+++ 20230825/_4470.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cin.ignore();
+    for (int i = 1; i <= n; i++)
+    {
+        string str;
+        getline(cin, str);
+        cout << i << ". " << str << '\n';
+    }
+}
