d98523a7721a70269849766dea81a6ef9335adfe
Minsik Kim <ppp37686@gmail.com>
2023-09-13 23:27:06 +0900

    added 11719

---
+++ 20230913/_11719.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str;
+    while(1)
+    {
+        getline(cin, str);
+        if(cin.eof()) break;
+        cout << str << '\n';
+    }
+}
